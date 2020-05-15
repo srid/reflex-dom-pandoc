92ba20295611451bbd142572de7a2ff325c932af
Sridhar Ratnakumar <srid@srid.ca>
2020-05-15 12:46:48 -0400

    Finish instances

--- src/Reflex/Dom/Pandoc/Document.hs
+++ src/Reflex/Dom/Pandoc/Document.hs
@@ -16,8 +16,9 @@ module Reflex.Dom.Pandoc.Document
   ( elPandocDoc,
     elPandocInlines,
     elPandocHeading1,
-    rawAsActual,
     rawAsRaw,
+    PandocBuilder,
+    RawHtml (..),
   )
 where
 
@@ -41,7 +42,7 @@ import Text.Pandoc.Definition
 
 class RawHtml m where
   type RawHtmlConstraints m :: Constraint
-  elRawHtml :: RawHtmlConstraints m => Text -> m ()
+  elRawHtml :: RawHtmlConstraints m => Format -> Text -> m ()
 
 instance RawHtml (StaticDomBuilderT t m) where
   type
@@ -57,86 +58,90 @@ instance RawHtml (StaticDomBuilderT t m) where
         PerformEvent t m,
         MonadReflexCreateTrigger t m
       )
-  elRawHtml :: RawHtmlConstraints (StaticDomBuilderT t m) => Text -> StaticDomBuilderT t m ()
-  elRawHtml s =
+  elRawHtml _f s =
     StaticDomBuilderT $ lift $ modify $ (:) $ fmap encodeUtf8Builder $ current $ constDyn s
 
-elPandocInlinesStatic ::
-  forall t m.
-  (RawHtmlConstraints (StaticDomBuilderT t m)) =>
-  [Inline] ->
-  StaticDomBuilderT t m ()
-elPandocInlinesStatic = void . flip runReaderT mempty . renderInlines (lift . (\(Format s) -> elRawHtml s))
+instance RawHtml m => RawHtml (ReaderT a m) where
+  type RawHtmlConstraints (ReaderT a m) = RawHtmlConstraints m
+  elRawHtml f s = ReaderT $ \_ -> elRawHtml f s
+
+instance RawHtml m => RawHtml (PostBuildT t m) where
+  type RawHtmlConstraints (PostBuildT t m) = RawHtmlConstraints m
+  elRawHtml f s = PostBuildT $ ReaderT $ \_ ->
+    elRawHtml f s
+
+type PandocBuilder t m =
+  ( DomBuilder t m,
+    RawHtml m,
+    RawHtmlConstraints m
+  )
 
 -- | Render list of Pandoc inlines
-elPandocInlines :: forall t m. DomBuilder t m => [Inline] -> m ()
-elPandocInlines = void . sansFootnotes . renderInlines rawAsRaw
+elPandocInlines :: forall t m. PandocBuilder t m => [Inline] -> m ()
+elPandocInlines = void . sansFootnotes . renderInlines
 
-type RenderRawF t m = DomBuilder t m => Format -> m ()
+type RenderRawF t m = PandocBuilder t m => Format -> Text -> m ()
 
 rawAsRaw :: RenderRawF t m
-rawAsRaw (Format s) =
-  el "pre" $ text s
+rawAsRaw (Format f) s =
+  elClass "pre" ("pandoc-raw " <> f) $ text s
 
-rawAsActual :: (Reflex t, Monad m) => Format -> StaticDomBuilderT t m ()
-rawAsActual (Format s) =
-  StaticDomBuilderT $ lift $ modify $ (:) $ fmap encodeUtf8Builder $ current $ constDyn s
-
-renderBlocks :: (MonadReader Footnotes m, DomBuilder t m) => (RenderRawF t m) -> [Block] -> m ()
-renderBlocks renderRaw =
-  mapM_ $ renderBlock renderRaw
+renderBlocks :: (MonadReader Footnotes m, PandocBuilder t m) => [Block] -> m ()
+renderBlocks =
+  mapM_ $ renderBlock
 
 -- | Convert Markdown to HTML
-elPandocDoc :: forall t m. DomBuilder t m => (RenderRawF t m) -> Pandoc -> m ()
-elPandocDoc renderRaw doc@(Pandoc _meta blocks) = do
+elPandocDoc :: forall t m. PandocBuilder t m => Pandoc -> m ()
+elPandocDoc doc@(Pandoc _meta blocks) = do
   let fs = getFootnotes doc
-  flip runReaderT fs $ renderBlocks (lift . renderRaw) blocks
-  renderFootnotes (sansFootnotes . renderBlocks (lift . renderRaw)) fs
+  flip runReaderT fs $ renderBlocks blocks
+  renderFootnotes (sansFootnotes . renderBlocks) fs
 
 -- | Render the first level of heading
-elPandocHeading1 :: DomBuilder t m => Pandoc -> m ()
+elPandocHeading1 :: PandocBuilder t m => Pandoc -> m ()
 elPandocHeading1 (Pandoc _meta blocks) = forM_ blocks $ \case
   Header 1 _ xs -> elPandocInlines xs
   _ -> blank
 
-renderBlock :: (MonadReader Footnotes m, DomBuilder t m) => (RenderRawF t m) -> Block -> m ()
-renderBlock renderRaw = \case
+renderBlock :: (MonadReader Footnotes m, PandocBuilder t m) => Block -> m ()
+renderBlock = \case
   -- Pandoc parses github tasklist as this structure.
-  Plain (Str "☐" : Space : is) -> checkboxEl False >> renderInlines renderRaw is
-  Plain (Str "☒" : Space : is) -> checkboxEl True >> renderInlines renderRaw is
-  Para (Str "☐" : Space : is) -> checkboxEl False >> renderInlines renderRaw is
-  Para (Str "☒" : Space : is) -> checkboxEl True >> renderInlines renderRaw is
+  Plain (Str "☐" : Space : is) -> checkboxEl False >> renderInlines is
+  Plain (Str "☒" : Space : is) -> checkboxEl True >> renderInlines is
+  Para (Str "☐" : Space : is) -> checkboxEl False >> renderInlines is
+  Para (Str "☒" : Space : is) -> checkboxEl True >> renderInlines is
   Plain xs ->
-    renderInlines renderRaw xs
+    renderInlines xs
   Para xs ->
-    el "p" $ renderInlines renderRaw xs
+    el "p" $ renderInlines xs
   LineBlock xss ->
     forM_ xss $ \xs -> do
-      renderInlines renderRaw xs <* text "\n"
+      renderInlines xs <* text "\n"
   CodeBlock attr x ->
     elCodeHighlighted attr x
-  RawBlock (Format t) x ->
+  RawBlock fmt x ->
     -- We are not *yet* sure what to do with this. For now, just dump it in pre.
     -- NOTE: if t==html, we must embed the raw HTML. But this doesn't seem
     -- possible reflex-dom without ghcjs constraints.
-    elClass "pre" ("pandoc-raw " <> t) $ text x
+    -- elClass "pre" ("pandoc-raw " <> t) $ text x
+    elRawHtml fmt x
   BlockQuote xs ->
-    el "blockquote" $ renderBlocks renderRaw xs
+    el "blockquote" $ renderBlocks xs
   OrderedList _lattr xss ->
     -- TODO: Implement list attributes.
     el "ol" $ do
       forM_ xss $ \xs -> do
-        el "li" $ renderBlocks renderRaw xs
+        el "li" $ renderBlocks xs
   BulletList xss ->
-    el "ul" $ forM_ xss $ \xs -> el "li" $ renderBlocks renderRaw xs
+    el "ul" $ forM_ xss $ \xs -> el "li" $ renderBlocks xs
   DefinitionList defs ->
     el "dl" $ forM_ defs $ \(term, descList) -> do
-      el "dt" $ renderInlines renderRaw term
+      el "dt" $ renderInlines term
       forM_ descList $ \desc ->
-        el "dd" $ renderBlocks renderRaw desc
+        el "dd" $ renderBlocks desc
   Header level attr xs ->
     elPandocAttr (headerElement level) attr $ do
-      renderInlines renderRaw xs
+      renderInlines xs
   HorizontalRule ->
     el "hr" blank
   Table _attr _captions _colSpec (TableHead _ hrows) tbodys _tfoot -> do
@@ -146,16 +151,16 @@ renderBlock renderRaw = \case
         forM_ hrows $ \(Row _ cells) -> do
           el "tr" $ do
             forM_ cells $ \(Cell _ _ _ _ blks) ->
-              el "th" $ renderBlocks renderRaw blks
+              el "th" $ renderBlocks blks
       forM_ tbodys $ \(TableBody _ _ _ rows) ->
         el "tbody" $ do
           forM_ rows $ \(Row _ cells) ->
             el "tr" $ do
               forM_ cells $ \(Cell _ _ _ _ blks) ->
-                el "td" $ renderBlocks renderRaw blks
+                el "td" $ renderBlocks blks
   Div attr xs ->
     elPandocAttr "div" attr $
-      renderBlocks renderRaw xs
+      renderBlocks xs
   Null ->
     blank
   where
@@ -171,30 +176,30 @@ renderBlock renderRaw = \case
           )
           blank
 
-renderInlines :: (MonadReader Footnotes m, DomBuilder t m) => (RenderRawF t m) -> [Inline] -> m ()
-renderInlines renderRaw =
-  mapM_ $ renderInline renderRaw
+renderInlines :: (MonadReader Footnotes m, PandocBuilder t m) => [Inline] -> m ()
+renderInlines =
+  mapM_ $ renderInline
 
-renderInline :: (MonadReader Footnotes m, DomBuilder t m) => (RenderRawF t m) -> Inline -> m ()
-renderInline renderRaw = \case
+renderInline :: (MonadReader Footnotes m, PandocBuilder t m) => Inline -> m ()
+renderInline = \case
   Str x ->
     text x
   Emph xs ->
-    el "em" $ renderInlines renderRaw xs
+    el "em" $ renderInlines xs
   Strong xs ->
-    el "strong" $ renderInlines renderRaw xs
+    el "strong" $ renderInlines xs
   Underline xs ->
-    el "u" $ renderInlines renderRaw xs
+    el "u" $ renderInlines xs
   Strikeout xs ->
-    el "strike" $ renderInlines renderRaw xs
+    el "strike" $ renderInlines xs
   Superscript xs ->
-    el "sup" $ renderInlines renderRaw xs
+    el "sup" $ renderInlines xs
   Subscript xs ->
-    el "sub" $ renderInlines renderRaw xs
+    el "sub" $ renderInlines xs
   SmallCaps xs ->
-    el "small" $ renderInlines renderRaw xs
+    el "small" $ renderInlines xs
   Quoted qt xs ->
-    flip inQuotes qt $ renderInlines renderRaw xs
+    flip inQuotes qt $ renderInlines xs
   Cite _ _ ->
     el "pre" $ text "error[reflex-doc-pandoc]: Pandoc Cite is not handled"
   Code attr x ->
@@ -218,10 +223,10 @@ renderInline renderRaw = \case
         elClass "span" "math display" $ text "$$" >> text s >> text "$$"
   Link attr xs (lUrl, lTitle) -> do
     let attr' = renderAttr attr <> ("href" =: lUrl <> "title" =: lTitle)
-    elAttr "a" attr' $ renderInlines renderRaw xs
+    elAttr "a" attr' $ renderInlines xs
   Image attr xs (iUrl, iTitle) -> do
     let attr' = renderAttr attr <> ("src" =: iUrl <> "title" =: iTitle)
-    elAttr "img" attr' $ renderInlines renderRaw xs
+    elAttr "img" attr' $ renderInlines xs
   Note xs -> do
     fs :: Footnotes <- ask
     case Map.lookup (Footnote xs) fs of
@@ -229,13 +234,13 @@ renderInline renderRaw = \case
         -- No footnote in the global map (this means that the user has
         -- defined a footnote inside a footnote); just put the whole thing in
         -- aside.
-        elClass "aside" "footnote-inline" $ renderBlocks renderRaw xs
+        elClass "aside" "footnote-inline" $ renderBlocks xs
       Just idx ->
         renderFootnoteRef idx
   -- el "aside" $ renderBlocks xs
   Span attr xs ->
     elPandocAttr "span" attr $
-      renderInlines renderRaw xs
+      renderInlines xs
   where
     inQuotes w = \case
       SingleQuote -> text "❛" >> w <* text "❜"
