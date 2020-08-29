87486d7b705a85951846c2ee2829ffbb92f290ef
Sridhar Ratnakumar <srid@srid.ca>
2020-08-29 16:44:31 -0400

    better types

--- src/Reflex/Dom/Pandoc/URILink.hs
+++ src/Reflex/Dom/Pandoc/URILink.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
--- TODO: Rename
 module Reflex.Dom.Pandoc.URILink where
 
+import Control.Monad (guard)
 import Data.Maybe
 import Text.Pandoc.Definition
 import qualified Text.Pandoc.Walk as W
@@ -11,18 +11,20 @@ import Text.URI (URI, mkURI)
 
 -- | A Pandoc Link node with a valid URI
 data URILink = URILink
-  { _uriLink_inner :: [Inline],
-    _uriLink_uri :: URI,
-    _uriLink_autolink :: Bool
+  { -- | This is set to Nothing for autolinks
+    _uriLink_inner :: Maybe [Inline],
+    _uriLink_uri :: URI
   }
   deriving (Eq, Show, Ord)
 
 uriLinkFromInline :: Inline -> Maybe URILink
 uriLinkFromInline = \case
-  Link _attr inner (url, _title) -> do
+  Link _attr inlines (url, _title) -> do
     uri <- mkURI url
-    let autolink = inner == [Str url]
-    pure $ URILink inner uri autolink
+    let inner = do
+          guard $ inlines /= [Str url]
+          pure inlines
+    pure $ URILink inner uri
   _ ->
     Nothing
 
